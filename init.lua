8863d463de9936a080a9da3941832866576a3d48
Roberto Flores <25ASAB015@ujmd.edu.sv>
2025-09-09 23:48:39 -0600

    feat(init): include autocmd and lazy plugin manager, add user configuration support

--- init.lua
+++ init.lua
@@ -1,12 +1,10 @@
 require('core.options')
 require('core.functions')
 require('core.keys')
-
-
-
-
-
-
+require('core.autocmd')
+require('plugins.lazy')
+-- Add user configs to this module
+pcall(require, 'user')
 
 -- ╔══════════════════════════════════════════════════════════════════════╗
 -- ║                                                                      ║
